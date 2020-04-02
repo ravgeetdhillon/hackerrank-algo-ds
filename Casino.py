ca3f96d3a670ab418b889c7a18df773aca5d0048
Ravgeet Dhillon <ravgeetdhillon@gmail.com>
2020-04-02 18:08:28 +0530

    changed spacing

--- Casino.py
+++ Casino.py
@@ -1,28 +1,28 @@
-variables=[int(i) for i in input().split(',')]
-m=variables[0]
-n=variables[1]
+variables = [int(i) for i in input().split(',')]
+m = variables[0]
+n = variables[1]
 
-matrix=[]
+matrix = []
 for i in range(m):
     matrix.append([])
     
 for i in range(n):
-    queue=[int(i) for i in input().split(',')]
-    n=0
+    queue = [int(i) for i in input().split(',')]
+    n = 0
     for i in queue:
         matrix[n].append(i)
-        n+=1
+        n += 1
 
-num=''
-while len(num)!=m*n:
-    max=-1
+num = ''
+while len(num) != m*n:
+    max = -1
     for i in range(m):
         try:
-            if matrix[i][-1]>max:
-                max=matrix[i][-1]
-                d=i
+            if matrix[i][-1] > max:
+                max = matrix[i][-1]
+                d = i
         except IndexError:
             continue
     del matrix[d][-1]
-    num+=str(max)
+    num += str(max)
     print(num)
